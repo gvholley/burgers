ae7a77c69dcb4844ac51deb83cc981d2a0fc1486
Grant Holley <gvholley@gmail.com>
2020-12-13 19:07:46 +0900

    Edited index

--- app/javascript/components/Burgers/Burgers.js
+++ app/javascript/components/Burgers/Burgers.js
@@ -1,10 +1,28 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Burgers = () => {
-  return (
-  <div>This is the Burgers#index view for our app.
+  const [burgers, setBurgers] = useState([])
 
+  useEffect(() => {
+
+    axios.get('/api/v1/burgers.json')
+    .then( resp => {
+      setBurgers(resp.data.data)
+    } )
+    .catch( resp => )
+
+  }, [burgers.length])
+
+  const list = burgers.map (item => {
+    return (<li key={item.attributes.name}>{item.attributes.name}</li>)
+  })
+  return (
+    <React.Fragment>
+  <div>This is the Burgers#index view for our app.
+  <ul>{list}</ul>
   </div>
+  </React.Fragment>
   )
 }
 
